b4b58a426663453ae93ceea14d0ce218bf566282
Sajal Tyagi <60937901+STK101@users.noreply.github.com>
2022-06-12 16:18:22 +0000

    added new function

---
+++ start_func.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import re
+from Levenshtein import distance as lev
+import math
+from collections import Counter
+from scipy.spatial.distance import pdist, squareform
+from collections import deque
+import scheduler
+
+def starter_ex(unsequenced_schedule, priority_present,file_name = 'output.xlsx' , k = 10, max_trials = 10000 ,shuffle = False):
+
+    #'--unsequenced_schedule' => "Path to the  unsequenced schedule excel file")
+    #'--priority_present'  => "if false adds a priority column with all tasks having equal priority" i.e. if priority column present in input then true else false
+    #'--file_name' => 'output.xlsx' ,help="name for the sequenced excel file")
+    #'--k' => "Number of best schedules that need to be present in the output file")
+    #'--max_trials' => "Max Swaps for the SA optimiser")
+    #'--shuffle' => "To shuffle the imported unscheduled file")
+    xls = pd.ExcelFile(unsequenced_schedule) # input file
+    file_name = file_name
+    df1 = pd.read_excel(xls, xls.sheet_names[0])
+    df1['DATE'] = pd.to_datetime(df1["DATE"], format='%Y-%m-%d', errors='coerce')
+    if (priority_present == 'False'):
+        df1['PRIORITY'] = 1
+    if (shuffle == 'True'):
+        shuffled = df1.sample(frac=1).reset_index(drop=True)
+    else:
+        shuffled = (df1.copy()).reset_index(drop = True)
+    
+    final = scheduler.priority_based_seperator_2(shuffled, k, max_trials)
+    with pd.ExcelWriter(file_name) as writer:
+        tc = len(final[0])
+        for i in range(0,tc):
+            ((final[0])[tc - i - 1]).to_excel(writer, sheet_name = 'S' + str(i) + 'CId- ' + str((final[1])[tc - i - 1]) , index=False)
