0f0548e726826b646fe0261f2090a7c49c20af6c
Sajal Tyagi <60937901+STK101@users.noreply.github.com>
2022-07-14 11:52:30 +0530

    Update start_func.py

--- start_func.py
+++ start_func.py
@@ -35,7 +35,7 @@ def starter_ex(unsequenced_schedule, file_name = 'output.xlsx' , k = 1, max_tria
     #'--shuffle' => "To shuffle the imported unscheduled file")
     #xls = pd.ExcelFile(unsequenced_schedule) # input file
     df1 = pd.read_csv(unsequenced_schedule)
-    df1['DATE'] = pd.to_datetime(df1["DATE"], format='%d-%b-%Y', errors='coerce') #'%d-%b-%Y' '%Y-%m-%d'
+    df1['DATE'] = pd.to_datetime(df1["DATE"], format='%d-%b-%y', errors='coerce') #'%d-%b-%Y' '%Y-%m-%d'
     u_dates = (df1['DATE']).unique()
     u_dates.sort()
     u_dict =  dict(zip(u_dates, range(0,len(u_dates))))
