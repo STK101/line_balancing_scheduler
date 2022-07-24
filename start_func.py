b88df9de5ebddc93a11c9aa9c7a4252bc2a8fe3c
Sajal Tyagi <60937901+STK101@users.noreply.github.com>
2022-07-24 21:32:03 +0000

    added xlsx input

--- start_func.py
+++ start_func.py
@@ -34,9 +34,9 @@ def starter_ex(unsequenced_schedule, file_name = 'output.xlsx' , k = 1, max_tria
     #'--k' => "Number of best schedules that need to be present in the output file")
     #'--max_trials' => "Max Swaps for the SA optimiser")
     #'--shuffle' => "To shuffle the imported unscheduled file")
-    #xls = pd.ExcelFile(unsequenced_schedule) # input file
-    df1 = unsequenced_schedule#pd.read_csv(unsequenced_schedule, index_col = 0)
-    df1['DATE'] = pd.to_datetime(df1["DATE"], format='%d-%b-%y', errors='coerce') #'%d-%b-%Y' '%Y-%m-%d'
+    xls = pd.ExcelFile(unsequenced_schedule) # input file
+    df1 = pd.read_csv(unsequenced_schedule, index_col = 0)
+    df1['DATE'] = pd.to_datetime(df1["DATE"], format='%Y-%m-%d', errors='coerce') #'%d-%b-%Y' '%Y-%m-%d'
     u_dates = (df1['DATE']).unique()
     u_dates.sort()
     u_dict =  dict(zip(u_dates, range(0,len(u_dates))))
